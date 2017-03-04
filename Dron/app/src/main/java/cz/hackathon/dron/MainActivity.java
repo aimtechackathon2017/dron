59c8cb0992ae179544ab14483760486647cbfeeb
Jan <jan.klik@futuretek.cz>
2017-03-04 04:53:16 +0100

    Tlačítka na vzlétnutí a přistání Doplnění API klíče

--- Dron/app/src/main/java/cz/hackathon/dron/MainActivity.java
+++ Dron/app/src/main/java/cz/hackathon/dron/MainActivity.java
@@ -1,30 +1,65 @@
 package cz.hackathon.dron;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import dji.common.error.DJIError;
 import dji.common.error.DJISDKError;
+import dji.common.util.DJICommonCallbacks;
 import dji.sdk.base.DJIBaseComponent;
 import dji.sdk.base.DJIBaseProduct;
+import dji.sdk.flightcontroller.DJIFlightController;
+import dji.sdk.products.DJIAircraft;
 import dji.sdk.sdkmanager.DJISDKManager;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getName();
     public static final String FLAG_CONNECTION_CHANGE = "dji_sdk_connection_change";
     private static DJIBaseProduct mProduct;
+    private static Button mButtonTakeOff;
+    private static Button mButtonLand;
     private Handler mHandler;
+    private DJIFlightController mFlightController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Initialize DJI SDK Manager
         mHandler = new Handler(Looper.getMainLooper());
+
+        mButtonTakeOff = (Button) findViewById(R.id.takeOff);
+        mButtonLand = (Button) findViewById(R.id.land);
+
+        mButtonTakeOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mFlightController.takeOff(new DJICommonCallbacks.DJICompletionCallback() {
+                        @Override
+                        public void onResult(DJIError djiError) {
+
+                        }
+                    });
+            }
+        });
+        mButtonLand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mFlightController.autoLanding(new DJICommonCallbacks.DJICompletionCallback() {
+                    @Override
+                    public void onResult(DJIError djiError) {
+
+                    }
+                });
+            }
+        });
         DJISDKManager.getInstance().initSDKManager(this, mDJISDKManagerCallback);
     }
 
@@ -57,7 +92,9 @@ public class MainActivity extends AppCompatActivity {
             mProduct = newProduct;
             if(mProduct != null) {
                 mProduct.setDJIBaseProductListener(mDJIBaseProductListener);
+                mFlightController = ((DJIAircraft) mProduct).getFlightController();
             }
+
             notifyStatusChange();
         }
     };
